20034d076d0cd5b8129a1595076c56c4160ae0a9
McGregorSt <stasiak.gregor@gmail.com>
2019-03-24 09:50:26 +0100

    initial work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './db/app2.json'
+
+import AppJSX from './components/App'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+     <div>
+       <AppJSX data={ data }/>
+     </div>
     );
   }
 }
